98d0eaae411c1dd05a8f56f0f9432cd0d83ca0dc
Googie2149 <gc2149@gmail.com>
2018-10-03 01:28:00 -0400

    extra logs

--- TaranzaSoul/Modules/Logger.cs
+++ TaranzaSoul/Modules/Logger.cs
@@ -33,6 +33,58 @@ namespace TaranzaSoul
         //private Dictionary<ulong, StoredMessage> MessageLogs = new Dictionary<ulong, StoredMessage>();
         private List<ulong> messagedUsers = new List<ulong>();
 
+        private void Log(Exception ex)
+        {
+            string exMessage;
+
+            if (ex != null)
+            {
+                while (ex is AggregateException && ex.InnerException != null)
+                    ex = ex.InnerException;
+                exMessage = $"{ex.Message}";
+                if (exMessage != "Reconnect failed: HTTP/1.1 503 Service Unavailable")
+                    exMessage += $"\n{ex.StackTrace}";
+            }
+            else
+                exMessage = null;
+
+            string sourceName = ex.Source?.ToString();
+
+            string text;
+            if (ex.Message == null)
+            {
+                text = exMessage ?? "";
+                exMessage = null;
+            }
+            else
+                text = ex.Message;
+
+            StringBuilder builder = new StringBuilder(text.Length + (sourceName?.Length ?? 0) + (exMessage?.Length ?? 0) + 5);
+            if (sourceName != null)
+            {
+                builder.Append('[');
+                builder.Append(sourceName);
+                builder.Append("] ");
+            }
+            builder.Append($"[{DateTime.Now.ToString("d")} {DateTime.Now.ToString("T")}] ");
+            for (int i = 0; i < text.Length; i++)
+            {
+                //Strip control chars
+                char c = text[i];
+                if (c == '\n' || !char.IsControl(c) || c != (char)8226)
+                    builder.Append(c);
+            }
+            if (exMessage != null)
+            {
+                builder.Append(": ");
+                builder.Append(exMessage);
+            }
+
+            text = builder.ToString();
+
+            Console.WriteLine(text);
+        }
+
         public async Task Install(IServiceProvider _services)
         {
             client = _services.GetService<DiscordSocketClient>();
@@ -99,88 +151,102 @@ namespace TaranzaSoul
 
         private async Task Client_UserLeft(SocketGuildUser user)
         {
-            if (user.Guild.Id == 132720341058453504)
+            try
             {
-                string message = $":door: " +
-                    $"**User Left** `{DateTime.Now.ToString("d")} {DateTime.Now.ToString("T")}`\n" +
-                    $"{user.Username}#{user.Discriminator} ({user.Id})" +
-                    ((user.JoinedAt.HasValue) ? $"\nOriginal Join Date `{user.JoinedAt.Value.ToLocalTime().ToString("d")} {user.JoinedAt.Value.ToLocalTime().ToString("T")}`" : "");
-
-                if (config.WatchedIds.ContainsKey(user.Id))
+                if (user.Guild.Id == 132720341058453504)
                 {
-                    message = $"{message}\n<@&451057945044582400> That user was flagged! {config.WatchedIds[user.Id]}";
-                }
+                    string message = $":door: " +
+                        $"**User Left** `{DateTime.Now.ToString("d")} {DateTime.Now.ToString("T")}`\n" +
+                        $"{user.Username}#{user.Discriminator} ({user.Id})" +
+                        ((user.JoinedAt.HasValue) ? $"\nOriginal Join Date `{user.JoinedAt.Value.ToLocalTime().ToString("d")} {user.JoinedAt.Value.ToLocalTime().ToString("T")}`" : "");
 
-                await (client.GetGuild(132720341058453504).GetChannel(267377140859797515) as ISocketMessageChannel)
-                    .SendMessageAsync(message);
+                    if (config.WatchedIds.ContainsKey(user.Id))
+                    {
+                        message = $"{message}\n<@&451057945044582400> That user was flagged! {config.WatchedIds[user.Id]}";
+                    }
+
+                    await (client.GetGuild(132720341058453504).GetChannel(267377140859797515) as ISocketMessageChannel)
+                        .SendMessageAsync(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
             }
         }
 
         private async Task Client_UserJoined(SocketGuildUser user)
         {
-            if (user.Guild.Id == 132720341058453504)
+            try
             {
-                string message = $":wave: " +
-                    $"**User Joined** `{DateTime.Now.ToString("d")} {DateTime.Now.ToString("T")}`\n" +
-                    $"{user.Username}#{user.Discriminator} ({user.Id}) ({user.Mention})\n" +
-                    $"**Account created** `{user.CreatedAt.ToLocalTime().ToString("d")} {user.CreatedAt.ToLocalTime().ToString("T")}`";
-
-                if (config.WatchedIds.ContainsKey(user.Id))
+                if (user.Guild.Id == 132720341058453504)
                 {
-                    message = $"{message}\n<@&451057945044582400> This user has been flagged! {config.WatchedIds[user.Id]}";
-                }
-
-                await (client.GetGuild(132720341058453504).GetChannel(267377140859797515) as ISocketMessageChannel)
-                    .SendMessageAsync(message);
-
-                
-                
-
-                //if (user.Id == 144980421501911040) // splash
-                //{
-                //    await user.AddRoleAsync(client.GetGuild(132720341058453504).GetRole(250643101675159552));
-                //}
-
-                if (user.Guild.VerificationLevel < VerificationLevel.Extreme)
-                    return;
-
-
-                if (user.CreatedAt.Date < DateTimeOffset.Now.AddDays(-14))
-                {
-                    Task.Run(async () =>
-                    {
-                        try
-                        {
-                            var role = client.GetGuild(132720341058453504).GetRole(346373986604810240);
-                            await user.SendMessageAsync(
-                                "Welcome to the Partnered /r/Kirby Discord Server!\n" +
-                                "To help ensure the peaceful atmosphere of the server, you'll have to wait about 10 minutes until you can see the rest of the channels, " +
-                                "but until then you can familiarize yourself with <#132720402727174144> and <#361565642027171841>. We hope you enjoy your stay!");
-
-                            await Task.Delay(1000 * 60 * 10); // wait 10 minutes to be closer to Discord's tier 3 verification level and give us a chance to react
-
-                            await user.AddRoleAsync(role);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Error adding role!\nMessage: {ex.Message}\nSource: {ex.Source}\n{ex.InnerException}");
-                        }
-                    });
-                }
-                else
-                {
-                    await Task.Delay(1000);
-
-                    await (client.GetGuild(132720341058453504).GetChannel(346371601564172298) as ISocketMessageChannel)
-                        .SendMessageAsync($"<:marxist_think:305877855366152193> " +
+                    string message = $":wave: " +
                         $"**User Joined** `{DateTime.Now.ToString("d")} {DateTime.Now.ToString("T")}`\n" +
                         $"{user.Username}#{user.Discriminator} ({user.Id}) ({user.Mention})\n" +
-                        $"**Account created** `{user.CreatedAt.ToLocalTime().ToString("d")} {user.CreatedAt.ToLocalTime().ToString("T")}`");
+                        $"**Account created** `{user.CreatedAt.ToLocalTime().ToString("d")} {user.CreatedAt.ToLocalTime().ToString("T")}`";
 
-                    await user.SendMessageAsync("Hi, welcome to the /r/Kirby Discord server! If you're seeing this, it means **your account is new**, and as such needs to be verified before you can participate in this server. " +
-                        "Toss us a mod mail on /r/Kirby with your Discord username and we'll get you set up as soon as we can https://www.reddit.com/message/compose?to=%2Fr%2FKirby" +
-                        "\n\nIf you do not have a Reddit account, or it's new/unused, your best bet is to register your phone number on Discord, then send an online moderator a message.");
+                    if (config.WatchedIds.ContainsKey(user.Id))
+                    {
+                        message = $"{message}\n<@&451057945044582400> This user has been flagged! {config.WatchedIds[user.Id]}";
+                    }
+
+                    await (client.GetGuild(132720341058453504).GetChannel(267377140859797515) as ISocketMessageChannel)
+                        .SendMessageAsync(message);
+
+
+
+
+                    //if (user.Id == 144980421501911040) // splash
+                    //{
+                    //    await user.AddRoleAsync(client.GetGuild(132720341058453504).GetRole(250643101675159552));
+                    //}
+
+                    if (user.Guild.VerificationLevel < VerificationLevel.Extreme)
+                        return;
+
+
+                    if (user.CreatedAt.Date < DateTimeOffset.Now.AddDays(-14))
+                    {
+                        Task.Run(async () =>
+                        {
+                            try
+                            {
+                                var role = client.GetGuild(132720341058453504).GetRole(346373986604810240);
+                                await user.SendMessageAsync(
+                                    "Welcome to the Partnered /r/Kirby Discord Server!\n" +
+                                    "To help ensure the peaceful atmosphere of the server, you'll have to wait about 10 minutes until you can see the rest of the channels, " +
+                                    "but until then you can familiarize yourself with <#132720402727174144> and <#361565642027171841>. We hope you enjoy your stay!");
+
+                                await Task.Delay(1000 * 60 * 10); // wait 10 minutes to be closer to Discord's tier 3 verification level and give us a chance to react
+
+                            await user.AddRoleAsync(role);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"Error adding role!\nMessage: {ex.Message}\nSource: {ex.Source}\n{ex.InnerException}");
+                            }
+                        });
+                    }
+                    else
+                    {
+                        await Task.Delay(1000);
+
+                        await (client.GetGuild(132720341058453504).GetChannel(346371601564172298) as ISocketMessageChannel)
+                            .SendMessageAsync($"<:marxist_think:305877855366152193> " +
+                            $"**User Joined** `{DateTime.Now.ToString("d")} {DateTime.Now.ToString("T")}`\n" +
+                            $"{user.Username}#{user.Discriminator} ({user.Id}) ({user.Mention})\n" +
+                            $"**Account created** `{user.CreatedAt.ToLocalTime().ToString("d")} {user.CreatedAt.ToLocalTime().ToString("T")}`");
+
+                        await user.SendMessageAsync("Hi, welcome to the /r/Kirby Discord server! If you're seeing this, it means **your account is new**, and as such needs to be verified before you can participate in this server. " +
+                            "Toss us a mod mail on /r/Kirby with your Discord username and we'll get you set up as soon as we can https://www.reddit.com/message/compose?to=%2Fr%2FKirby" +
+                            "\n\nIf you do not have a Reddit account, or it's new/unused, your best bet is to register your phone number on Discord, then send an online moderator a message.");
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
             }
         }
 
