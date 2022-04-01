b7a53701c491458ce3505ebdb5421826844963be
Googie2149 <gc2149@gmail.com>
2022-04-01 12:55:43 -0400

    debugggggggg

--- TaranzaSoul/Modules/Standard.cs
+++ TaranzaSoul/Modules/Standard.cs
@@ -598,6 +598,8 @@ namespace TaranzaSoul.Modules.Standard
             EmbedBuilder builder = new EmbedBuilder();
             builder.WithColor(0x2a28a5);
 
+            Console.WriteLine("1");
+
             if (debug == "0" && logger.LastFightTime >= DateTimeOffset.Now.AddMinutes(-1))
             {
                 builder.WithDescription($"Lord Meta Knight is preparing for the next fight. You'll have your chance soon.");
@@ -605,30 +607,40 @@ namespace TaranzaSoul.Modules.Standard
                 return;
             }
 
+            Console.WriteLine("2");
+
             if (debug == "0" && logger.FightCooldown[Context.User.Id] >= DateTimeOffset.Now.AddMinutes(-60))
             {
                 TimeSpan t = logger.FightCooldown[Context.User.Id] - DateTimeOffset.Now.AddMinutes(-60);
 
+                Console.WriteLine("3");
+
                 //Task.Run(async () =>
                 //{
 
-                    builder.WithDescription($"Lord Meta Knight is currently fighting other challengers. You'll be able to have have your rematch in {t.Minutes:0}:{t.Seconds:00}.")
+                builder.WithDescription($"Lord Meta Knight is currently fighting other challengers. You'll be able to have have your rematch in {t.Minutes:0}:{t.Seconds:00}.")
                     .WithThumbnailUrl("cooldown.png");
 
                 await Context.Channel.SendFileAsync("./Images/cooldown.png", embed: builder.Build());
 
-                    //var msg = await Context.Channel.SendMessageAsync($" (Try again after {t.Minutes:0}:{t.Seconds:00})");
+                Console.WriteLine("4");
 
-                    //await Task.Delay(1000 * 3);
+                //var msg = await Context.Channel.SendMessageAsync($" (Try again after {t.Minutes:0}:{t.Seconds:00})");
 
-                    //await msg.DeleteAsync();
-                    //await Context.Message.DeleteAsync();
+                //await Task.Delay(1000 * 3);
+
+                //await msg.DeleteAsync();
+                //await Context.Message.DeleteAsync();
                 //});
 
                 return;
             }
 
+            Console.WriteLine("5");
+
             int result = RandomInteger(0, 100);
+
+            Console.WriteLine("6");
 
             if (Context.User.Id == 102528327251656704 && debug != "0")
             {
@@ -636,25 +648,33 @@ namespace TaranzaSoul.Modules.Standard
                     result = 62;
                 else
                     result = 4;
+
+                Console.WriteLine("30000");
             }
             else 
             {
                 logger.FightCooldown[Context.User.Id] = DateTimeOffset.Now;
                 logger.LastFightTime = DateTimeOffset.Now;
+                Console.WriteLine("7");
             }
 
             bool milestone = false;
             string image = "";
 
+            Console.WriteLine("8");
+
             if (result == 62)
             {
+                Console.WriteLine("1342");
                 builder.WithDescription("Ngh... Damn! Impossible! You win this time...").WithThumbnailUrl("metaknightlose.png");
                 milestone = true;
                 image = "./Images/metaknightlose.png";
             }
             else
             {
+                Console.WriteLine("9");
                 int response = RandomInteger(0, 12);
+                Console.WriteLine("10");
 
                 string[] responses = new string[] {"You lose. Come back when you can put up a fight.",
                     "The victory is mine. Return when you're more fit to challenge me.",
@@ -668,9 +688,13 @@ namespace TaranzaSoul.Modules.Standard
                     "You are no match for Galaxia. Concede.",
                     "You fail. Such is the fate that befalls all who oppose me."};
 
+                Console.WriteLine("11");
+
                 builder.WithDescription(responses[response]).WithThumbnailUrl("metaknightwin.png");
                 image = "./Images/metaknightwin.png";
             }
+
+            Console.WriteLine("12");
 
             var msg = await Context.Channel.SendFileAsync(image, embed: builder.Build());
 
