9ae1f3f96dc830048f21ba438db73a09337a6d5e
Googie2149 <gc2149@gmail.com>
2022-06-10 03:39:36 -0400

    update dependencies, bump to dotnet 6

---
+++ TaranzaSoul/Services/ModMailService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Diagnostics;
+using NodaTime;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using System.Reflection;
+using System.IO;
+using RestSharp;
+using Newtonsoft.Json;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
+
+namespace TaranzaSoul.Services
+{
+    public class ModMailService
+    {
+        private DiscordSocketClient socketClient;
+        private DiscordSocketRestClient restClient;
+        private IServiceProvider services;
+        private Config config;
+
+        public async Task Install(IServiceProvider _services)
+        {
+            socketClient = _services.GetService<DiscordSocketClient>();
+            restClient = _services.GetService<DiscordSocketRestClient>();
+            config = _services.GetService<Config>();
+            services = _services;
+        }
+
+    }
+}
